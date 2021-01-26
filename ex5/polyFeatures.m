6e24335b17a6c7d154c063104b95f75e7bda5a60
Chris F <cewfowler@gmail.com>
2021-01-25 20:23:27 -0600

    Calculate validation curve values for various lambdas

--- ex5/polyFeatures.m
+++ ex5/polyFeatures.m
@@ -3,8 +3,6 @@ function [X_poly] = polyFeatures(X, p)
 %   [X_poly] = POLYFEATURES(X, p) takes a data matrix X (size m x 1) and
 %   maps each example into its polynomial features where
 %   X_poly(i, :) = [X(i) X(i).^2 X(i).^3 ...  X(i).^p];
-%
-
 
 % Initialize X_poly
 X_poly = zeros(numel(X), p);
